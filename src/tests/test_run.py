1ba60d9f7af8c4262bc1a8f8c93e8c0807a43825
DaveOkpare <okparedave@gmail.com>
2023-03-15 01:14:46 +0100

    Cleanup test_run

--- src/tests/test_run.py
+++ src/tests/test_run.py
@@ -14,31 +14,30 @@ from sql.run import (
 
 
 @pytest.fixture
-def mock_config_is_pandas():
-    class Config:
-        autopandas = True
-        autopolars = False
-
-    return Config
-
-
-@pytest.fixture
-def mock_config_is_polars():
-    class Config:
-        autopandas = False
-        autopolars = True
-
-    return Config
-
-
-@pytest.fixture
 def mock_config():
     class Config:
-        autopandas = False
-        autopolars = False
+        autopandas = None
+        autopolars = None
+        autocommit = None
         feedback = True
 
     return Config
+
+
+@pytest.fixture
+def mock_config_is_pandas(mock_config):
+    mock_config.autopandas = True
+    mock_config.autopolars = False
+
+    return mock_config
+
+
+@pytest.fixture
+def mock_config_is_polars(mock_config):
+    mock_config.autopandas = False
+    mock_config.autopolars = True
+
+    return mock_config
 
 
 @pytest.fixture
@@ -66,9 +65,9 @@ def test_is_postgres_or_redshift(dialect):
     assert is_postgres_or_redshift(dialect) is True
 
 
-def test_handle_postgres_special():
+def test_handle_postgres_special(clean_conns):
     with pytest.raises(ImportError):
-        handle_postgres_special("conn", "statement")
+        handle_postgres_special(clean_conns, "\\")
 
 
 @mock.patch("sql.run.select_df_type")
@@ -96,19 +95,18 @@ def test_select_df_type_is_none(mock_select_df_type, mock_config, mock_result_se
     assert isinstance(result, type(mock_result_set))
 
 
-def test_sql_begin_exception(clean_conns):
+def test_sql_begin_exception(clean_conns, mock_config):
     with pytest.raises(Exception):
-        run(clean_conns, "BEGIN", "config", "user_namespace")
+        run(clean_conns, "BEGIN", mock_config, "user_namespace")
 
 
-def test_sql_empty(mock_conns):
+def test_sql_empty(mock_conns, mock_config):
     assert (
-        run(mock_conns, "", "config", "user_namespace")
+        run(mock_conns, "", mock_config, "user_namespace")
         == "Connected: %s" % mock_conns.name
     )
 
 
-@mock.patch("sql.run.select_df_type")
 @mock.patch("sql.run.ResultSet")
 @mock.patch("sql.run.interpret_rowcount")
 @mock.patch("sql.run._commit")
@@ -118,7 +116,6 @@ def test_handle_postgres_special_is_called(
     mock__commit,
     mock_interpret_rowcount,
     mock_resultset_cls,
-    mock_select_df_type,
     mock_conns,
     mock_config,
 ):
@@ -130,6 +127,3 @@ def test_handle_postgres_special_is_called(
 
     mock_handle_postgres_special.assert_called()
     mock_handle_postgres_special.assert_called_once_with(mock_conns, "\\")
-    mock__commit.assert_called()
-    mock_resultset_cls.assert_called()
-    mock_select_df_type.assert_called()
