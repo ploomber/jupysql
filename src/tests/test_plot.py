68e39c8c6965f1ff1464054dd3e2e19e24f99f6d
Palash Shah <35114859+Palashio@users.noreply.github.com>
2023-03-19 00:45:03 -0400

    revert changes

--- src/tests/test_plot.py
+++ src/tests/test_plot.py
@@ -47,9 +47,9 @@ class DictOfFloats(Mapping):
 
 def test_boxplot_stats(chinook_db):
     con = duckdb.connect(database=":memory:")
-    con.execute(sqlalchemy.sql.text("INSTALL 'sqlite_scanner';"))
-    con.execute(sqlalchemy.sql.text("LOAD 'sqlite_scanner';"))
-    con.execute(sqlalchemy.sql.text(f"CALL sqlite_attach({chinook_db!r});"))
+    con.execute("INSTALL 'sqlite_scanner';")
+    con.execute("LOAD 'sqlite_scanner';")
+    con.execute(f"CALL sqlite_attach({chinook_db!r});")
 
     res = con.execute(sqlalchemy.sql.text("SELECT * FROM Invoice"))
     X = res.df().Total
