14a4dd1a4344e73015c285f56b2f8bb83eee3a7c
Mohit Mehta <mehtamohit013@gmail.com>
2023-03-23 13:39:22 -0400

    Added more tests for MSSQL

--- src/tests/integration/test_mssql.py
+++ src/tests/integration/test_mssql.py
@@ -1,4 +1,6 @@
 import pyodbc
+import pytest
+from matplotlib import pyplot as plt
 
 
 def test_query_count(ip_with_MSSQL):
@@ -6,14 +8,24 @@ def test_query_count(ip_with_MSSQL):
     out = ip_with_MSSQL.run_line_magic(
         "sql",
         """
-        SELECT *
+        SELECT TOP 3 *
         FROM taxi
-        ORDER BY 1
-        OFFSET 0 ROWS FETCH NEXT 3 ROWS ONLY
         """,
     )
 
     assert len(out) == 3
+
+
+@pytest.mark.xfail(reason="Parsing error CTE due to sqlglot")
+def test_cte(ip_with_MSSQL):
+    ip_with_MSSQL.run_cell(
+        "%sql --save taxi_subset --no-execute \
+        SELECT TOP 3 * FROM taxi "
+    )
+    out_query_with_save_arg = ip_with_MSSQL.run_cell(
+        "%sql --with taxi_subset SELECT * FROM taxi_subset"
+    )
+    assert len(out_query_with_save_arg.result) == 3
 
 
 def test_create_table_with_indexed_df(ip_with_MSSQL):
@@ -26,10 +38,8 @@ def test_create_table_with_indexed_df(ip_with_MSSQL):
     # Prepare DF
     ip_with_MSSQL.run_cell(
         """results = %sql\
-                    SELECT *\
-                    FROM taxi\
-                    ORDER BY 1\
-                    OFFSET 0 ROWS FETCH NEXT 15 ROWS ONLY
+                    SELECT TOP 15 *\
+                    FROM taxi
                     """
     )
     ip_with_MSSQL.run_cell("new_table_from_df = results.DataFrame()")
@@ -38,3 +48,61 @@ def test_create_table_with_indexed_df(ip_with_MSSQL):
     query_out = ip_with_MSSQL.run_cell("%sql SELECT * FROM new_table_from_df")
     assert persist_out.error_in_exec is None and query_out.error_in_exec is None
     assert len(query_out.result) == 15
+
+
+@pytest.mark.xfail(reason="Parsing error CTE due to sqlglot")
+@pytest.mark.parametrize(
+    "cell",
+    [
+        ("%sqlplot histogram --table plot_something --column x"),
+        ("%sqlplot hist --table plot_something --column x"),
+        ("%sqlplot histogram --table plot_something --column x --bins 10"),
+    ],
+    ids=[
+        "histogram",
+        "hist",
+        "histogram-bins",
+    ],
+)
+def test_sqlplot_histogram(ip_with_MSSQL, cell):
+
+    # clean current Axes
+    plt.cla()
+
+    ip_with_MSSQL.run_cell(
+        "%sql --save plot_something_subset"
+        " --no-execute SELECT TOP 3 * from plot_something "
+    )
+    out = ip_with_MSSQL.run_cell(cell)
+
+    assert type(out.result).__name__ in {"Axes", "AxesSubplot"}
+
+
+@pytest.mark.xfail(reason="Parsing error CTE due to sqlglot")
+@pytest.mark.parametrize(
+    "cell",
+    [
+        "%sqlplot boxplot --table plot_something --column x",
+        "%sqlplot box --table plot_something --column x",
+        "%sqlplot boxplot --table plot_something --column x --orient h",
+        "%sqlplot boxplot --with plot_something_subset --table "
+        "plot_something_subset --column x",
+    ],
+    ids=[
+        "boxplot",
+        "box",
+        "boxplot-horizontal",
+        "boxplot-with",
+    ],
+)
+def test_sqlplot_boxplot(ip_with_MSSQL, cell):
+    # clean current Axes
+    plt.cla()
+    ip_with_MSSQL.run_cell(
+        "%sql --save plot_something_subset"
+        " --no-execute SELECT TOP 3 * from plot_something"
+    )
+
+    out = ip_with_MSSQL.run_cell(cell)
+
+    assert type(out.result).__name__ in {"Axes", "AxesSubplot"}
