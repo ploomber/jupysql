8abcc715638974274309c42a3746a0d9e6482474
idomi <michael.ido@gmail.com>
2023-03-31 15:06:57 -0400

    Test commit

--- src/tests/test_column_guesser.py
+++ src/tests/test_column_guesser.py
@@ -3,7 +3,7 @@ import pytest
 from sql.magic import SqlMagic
 from IPython.core.interactiveshell import InteractiveShell
 
-ip = InteractiveShell()
+ip = InteractiveShell()  # lnslkfn
 
 
 class SqlEnv(object):
