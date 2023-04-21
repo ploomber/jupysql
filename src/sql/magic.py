37ebe5074db3add4372db6cf96788042160c3563
Eduardo Blancas <github@blancas.io>
2023-04-21 10:16:09 -0600

    changes due to review

--- src/sql/magic.py
+++ src/sql/magic.py
@@ -25,7 +25,7 @@ from sql.store import store
 from sql.command import SQLCommand
 from sql.magic_plot import SqlPlotMagic
 from sql.magic_cmd import SqlCmdMagic
-from sql._patch import patch_ipython
+from sql._patch import patch_ipython_usage_error
 from ploomber_core.dependencies import check_installed
 
 from traitlets.config.configurable import Configurable
@@ -457,7 +457,7 @@ def load_ipython_extension(ip):
     ip.register_magics(SqlPlotMagic)
     ip.register_magics(SqlCmdMagic)
 
-    patch_ipython(ip)
+    patch_ipython_usage_error(ip)
 
 
 # %%
