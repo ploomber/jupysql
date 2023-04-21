44dd0f1de5d54b8c73c5c5b09914cdb5d1bb3575
Eduardo Blancas <github@blancas.io>
2023-04-21 17:10:58 -0600

    adds more custom exceptions

--- src/sql/exceptions.py
+++ src/sql/exceptions.py
@@ -1,13 +1,17 @@
 from IPython.core import error
 
 
-def ArgumentError(message):
-    exc = error.UsageError(message)
-    exc.error_type = "ArgumentError"
-    return exc
+def exception_factory(name):
+    def _error(message):
+        exc = error.UsageError(message)
+        exc.error_type = name
+        return exc
+
+    return _error
 
 
-def TableNotFoundError(message):
-    exc = error.UsageError(message)
-    exc.error_type = "TableNotFoundError"
-    return exc
+ArgumentError = exception_factory("ArgumentError")
+TableNotFoundError = exception_factory("TableNotFoundError")
+MissingPackageError = exception_factory("MissingPackageError")
+TypeError = exception_factory("TypeError")
+RuntimeError = exception_factory("RuntimeError")
