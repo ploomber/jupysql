13cedc71bd212777bf6b7aa90b4d3e8da45752b4
Eduardo Blancas <github@blancas.io>
2023-04-24 19:09:51 -0600

    documents when to raise certaine xceptions

--- src/sql/exceptions.py
+++ src/sql/exceptions.py
@@ -13,9 +13,22 @@ def exception_factory(name):
     return _error
 
 
+# raise it when there's an issue with the user's input in a magic. e.g., missing an
+# argument
 UsageError = exception_factory("UsageError")
-TableNotFoundError = exception_factory("TableNotFoundError")
+
+# raise it when a user wants to use a feature that requires an optional dependency
 MissingPackageError = exception_factory("MissingPackageError")
+
+# the following exceptions should be called instead of the Python built-in ones,
+# for guidelines on when to use them:
+# https://docs.python.org/3/library/exceptions.html#bltin-exceptions
 TypeError = exception_factory("TypeError")
 RuntimeError = exception_factory("RuntimeError")
 ValueError = exception_factory("ValueError")
+
+
+# The following are internal exceptions that should not be raised directly
+
+# raised internally when the user chooses a table that doesn't exist
+TableNotFoundError = exception_factory("TableNotFoundError")
