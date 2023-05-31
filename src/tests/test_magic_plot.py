548890f7cf7e7febd43ed7dd98518d6602e99567
Mohit Mehta <mehtamohit013@gmail.com>
2023-05-31 14:18:15 -0400

    Added null test

--- src/tests/test_magic_plot.py
+++ src/tests/test_magic_plot.py
@@ -228,12 +228,34 @@ x
     ip.run_cell("%sql duckdb://")
 
 
+@pytest.fixture
+def load_data_one_col_null(ip):
+    if not Path("data_one.csv").is_file():
+        Path("data_one.csv").write_text(
+            """\
+x
+
+0
+
+0
+1
+
+1
+1
+2
+"""
+        )
+    ip.run_cell("%sql duckdb://")
+
 @_cleanup_cm()
 @image_comparison(baseline_images=["bar_one_col"], extensions=["png"], remove_text=True)
 def test_bar_one_col(load_data_one_col, ip):
-    # plt.cla()
     ip.run_cell("%sqlplot bar -t data_one.csv -c x")
 
+@_cleanup_cm()
+@image_comparison(baseline_images=["bar_one_col"], extensions=["png"], remove_text=True)
+def test_bar_one_col_null(load_data_one_col_null, ip):
+    ip.run_cell("%sqlplot bar -t data_one.csv -c x")
 
 @_cleanup_cm()
 @image_comparison(
@@ -268,6 +290,11 @@ def test_bar_two_col(load_data_two_col, ip):
 @_cleanup_cm()
 @image_comparison(baseline_images=["pie_one_col"], extensions=["png"], remove_text=True)
 def test_pie_one_col(load_data_one_col, ip):
+    ip.run_cell("%sqlplot pie -t data_one.csv -c x")
+
+@_cleanup_cm()
+@image_comparison(baseline_images=["pie_one_col"], extensions=["png"], remove_text=True)
+def test_pie_one_col_null(load_data_one_col_null, ip):
     ip.run_cell("%sqlplot pie -t data_one.csv -c x")
 
 
