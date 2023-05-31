54afbefe09573c48a2ae24068cc9948d50763f9f
Mohit Mehta <mehtamohit013@gmail.com>
2023-05-31 14:50:46 -0400

    Format

--- src/tests/test_magic_plot.py
+++ src/tests/test_magic_plot.py
@@ -255,7 +255,9 @@ def test_bar_one_col(load_data_one_col, ip):
 
 
 @_cleanup_cm()
-@image_comparison(baseline_images=["bar_one_col_null"], extensions=["png"], remove_text=True)
+@image_comparison(
+    baseline_images=["bar_one_col_null"], extensions=["png"], remove_text=True
+)
 def test_bar_one_col_null(load_data_one_col_null, ip):
     ip.run_cell("%sqlplot bar -t data_one_null.csv -c x")
 
@@ -297,7 +299,9 @@ def test_pie_one_col(load_data_one_col, ip):
 
 
 @_cleanup_cm()
-@image_comparison(baseline_images=["pie_one_col_null"], extensions=["png"], remove_text=True)
+@image_comparison(
+    baseline_images=["pie_one_col_null"], extensions=["png"], remove_text=True
+)
 def test_pie_one_col_null(load_data_one_col_null, ip):
     ip.run_cell("%sqlplot pie -t data_one_null.csv -c x")
 
