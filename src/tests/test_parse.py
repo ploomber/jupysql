4b5342bb9d9e8bac3c25f137451e335e28f7b5be
Anupam <anupam@Anupams-MBP.lan>
2023-06-24 00:22:21 -0400

    pytest

--- src/tests/test_parse.py
+++ src/tests/test_parse.py
@@ -90,8 +90,8 @@ def test_parse_shovel_operator():
         "dest=<< SELECT * FROM work",
         "dest=<<SELECT * FROM work",
         "dest=<<                   SELECT * FROM work",
-        "dest =   << select * FROM work",
-        "dest =   <<select * FROM work",
+        "dest =   << SELECT * FROM work",
+        "dest =   <<SELECT * FROM work",
     ],
 )
 def test_parse_return_shovel_operator(input_string, ip):
