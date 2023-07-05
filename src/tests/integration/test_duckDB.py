de19c42307f0b9fc519e65460d3ea193f8e18fa5
Mohit Mehta <mehtamohit013@gmail.com>
2023-07-05 12:12:55 -0400

    Fix

--- src/tests/integration/test_duckDB.py
+++ src/tests/integration/test_duckDB.py
@@ -5,14 +5,14 @@ def test_auto_commit_mode_on(ip_with_duckDB, caplog):
     with caplog.at_level(logging.DEBUG):
         ip_with_duckDB.run_cell("%config SqlMagic.autocommit=True")
         ip_with_duckDB.run_cell("%sql CREATE TABLE weather4 (city VARCHAR,);")
-    assert caplog.record_tuples[0] == "root"
-    assert caplog.record_tuples[1] == logging.DEBUG
+    assert caplog.record_tuples[0][0] == "root"
+    assert caplog.record_tuples[0][1] == logging.DEBUG
     assert (
         "The database driver doesn't support such AUTOCOMMIT" in caplog.record_tuples[2]
     )
     assert (
         "CustomSession' object has no attribute '_has_events'"
-        in caplog.record_tuples[2]
+        in caplog.record_tuples[0][2]
     )
 
 
