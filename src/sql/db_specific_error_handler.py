f6cffb04bf7582d3519ea84327628add28488952
neelasha23 <neelasha.sen@gmail.com>
2023-08-07 18:21:08 +0530

    statement

--- src/sql/db_specific_error_handler.py
+++ src/sql/db_specific_error_handler.py
@@ -42,7 +42,7 @@ def _is_table_not_found_error(error):
     )
 
 
-def detail_db(original_error):
+def detail(original_error):
     original_error = str(original_error)
     if any(msg in original_error for msg in DB_ERRORS) and (
         _is_syntax_error(original_error) or _is_table_not_found_error(original_error)
