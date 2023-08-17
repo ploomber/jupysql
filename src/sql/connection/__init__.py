7ee92a95c579b8fbecc5b741a15cd5aca2789ff9
Tony Kuo <yen.c@ploomber.io>
2023-08-17 11:45:42 -0400

    remove AbstractConnection

--- src/sql/connection/__init__.py
+++ src/sql/connection/__init__.py
@@ -1,6 +1,5 @@
 from sql.connection.connection import (
     ConnectionManager,
-    AbstractConnection,
     SQLAlchemyConnection,
     DBAPIConnection,
     is_pep249_compliant,
@@ -12,7 +11,6 @@ from sql.connection.connection import (
 
 __all__ = [
     "ConnectionManager",
-    "AbstractConnection",
     "SQLAlchemyConnection",
     "DBAPIConnection",
     "is_pep249_compliant",
