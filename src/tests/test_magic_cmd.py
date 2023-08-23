e7f6c68eac6f1617ad6b54974ea3d2d378af3fb9
SangGyu An <bbeat2782@gmail.com>
2023-08-23 08:26:35 -0700

    fix

--- src/tests/test_magic_cmd.py
+++ src/tests/test_magic_cmd.py
@@ -1,7 +1,6 @@
 import sys
 import math
 import pytest
-import os
 from IPython.core.error import UsageError
 from pathlib import Path
 
@@ -796,9 +795,7 @@ drivername = duckdb
 def test_connect_with_connections_ini(tmp_empty, ip_empty, file_content, stored_conns):
     Path("connections.ini").write_text(file_content)
     ip_empty.run_cell("%load_ext sql")
-    current_dir = os.getcwd()
-    connections_path = os.path.join(current_dir, "connections.ini")
-    ip_empty.run_cell(f"%config SqlMagic.dsn_filename = '{connections_path}'")
+    ip_empty.run_cell("%config SqlMagic.dsn_filename = './connections.ini'")
     connector_widget = ip_empty.run_cell("%sqlcmd connect").result
     assert isinstance(connector_widget, ConnectorWidget)
     assert connector_widget.stored_connections == stored_conns
@@ -806,9 +803,7 @@ def test_connect_with_connections_ini(tmp_empty, ip_empty, file_content, stored_
 
 def test_connect_when_no_connections_ini(tmp_empty, ip_empty):
     ip_empty.run_cell("%load_ext sql")
-    current_dir = os.getcwd()
-    connections_path = os.path.join(current_dir, "connections.ini")
-    ip_empty.run_cell(f"%config SqlMagic.dsn_filename = '{connections_path}'")
+    ip_empty.run_cell("%config SqlMagic.dsn_filename = './connections.ini'")
     connector_widget = ip_empty.run_cell("%sqlcmd connect").result
     assert isinstance(connector_widget, ConnectorWidget)
     assert connector_widget.stored_connections == []
