1d85d85de4660d1162fa0aefe619f2c2416b4f19
neelasha23 <neelasha.sen@gmail.com>
2023-09-27 18:15:13 +0530

    pandas version

--- setup.py
+++ setup.py
@@ -33,14 +33,14 @@ install_requires = [
 DEV = [
     "flake8",
     "pytest",
-    "pandas",
+    "pandas==2.0.3",
     "polars==0.17.2",  # 04/18/23 this breaks our CI
     "pyarrow",
     "invoke",
     "pkgmt",
     "twine",
     # tests
-    "duckdb",
+    "duckdb<0.9.0",
     "duckdb-engine",
     "pyodbc",
     # sql.plot module tests
