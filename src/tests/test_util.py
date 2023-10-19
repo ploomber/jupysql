61ae856581533a84d863d18ac399abc9dea9fd11
Vineet Agarwal <vineetagarwal2402@gmail.com>
2023-10-19 16:20:05 -0700

    added tests

--- src/tests/test_util.py
+++ src/tests/test_util.py
@@ -10,6 +10,37 @@ EXPECTED_STORE_SUGGESTIONS = (
 )
 
 
+@pytest.fixture
+def check_duplicate_message_factory():
+    def _generate_error_message(cmd, args, aliases=None):
+        error_message = ""
+        duplicates = set([arg for arg in args if args.count(arg) != 1])
+
+        if duplicates:
+            error_message += (
+                f"Duplicate arguments in %{cmd}. "
+                "Please use only one of each of the following: "
+                f"{', '.join(sorted(duplicates))}."
+            )
+            if aliases:
+                error_message += " "
+
+        if aliases:
+            alias_list = []
+            for pair in sorted(aliases):
+                print(pair[0], pair[1])
+                alias_list.append(f"{f'-{pair[0]}'} or {f'--{pair[1]}'}")
+            error_message += (
+                f"Duplicate aliases for arguments in %{cmd}. "
+                "Please use either one of "
+                f"{', '.join(alias_list)}."
+            )
+
+        return error_message
+
+    return _generate_error_message
+
+
 @pytest.mark.parametrize(
     "store_table, query",
     [
@@ -147,41 +178,372 @@ def test_is_sqlalchemy_error(string, substrings, expected):
 
 
 @pytest.mark.parametrize(
-    "args, cmd_from, expected_error_message",
+    "cmd_from, args, aliases",
     [
+        # FOR SQL
+        #
+        # for creator/c
         (
-            ["--table", "--table"],
-            "sqlcmd",
-            "Duplicate arguments in %sqlcmd. \
-Please use only one of each of the following: --table",
-        ),
-        (
-            ["--alias", "--alias"],
             "sql",
-            "Duplicate arguments in %sql. \
-Please use only one of each of the following: --alias",
+            ["--creator", "--creator"],
+            [],
         ),
         (
-            ["--table", "--table", "--column", "--column"],
+            "sql",
+            ["-c", "-c"],
+            [],
+        ),
+        (
+            "sql",
+            ["--creator", "-c"],
+            [("c", "creator")],
+        ),
+        # for persist/p
+        (
+            "sql",
+            ["--persist", "--persist"],
+            [],
+        ),
+        (
+            "sql",
+            ["-p", "-p"],
+            [],
+        ),
+        (
+            "sql",
+            ["--persist", "-p"],
+            [("p", "persist")],
+        ),
+        # for no-index/n
+        (
+            "sql",
+            ["--persist", "--no-index", "--no-index"],
+            [],
+        ),
+        (
+            "sql",
+            ["--persist", "-n", "-n"],
+            [],
+        ),
+        (
+            "sql",
+            ["--persist", "--no-index", "-n"],
+            [("n", "no-index")],
+        ),
+        # for file/f
+        (
+            "sql",
+            ["--file", "--file"],
+            [],
+        ),
+        (
+            "sql",
+            ["-f", "-f"],
+            [],
+        ),
+        (
+            "sql",
+            ["--file", "-f"],
+            [("f", "file")],
+        ),
+        # for save/S
+        (
+            "sql",
+            ["--save", "--save"],
+            [],
+        ),
+        (
+            "sql",
+            ["-S", "-S"],
+            [],
+        ),
+        (
+            "sql",
+            ["--save", "-S"],
+            [("S", "save")],
+        ),
+        # for alias/A
+        (
+            "sql",
+            ["--alias", "--alias"],
+            [],
+        ),
+        (
+            "sql",
+            ["-A", "-A"],
+            [],
+        ),
+        (
+            "sql",
+            ["--alias", "-A"],
+            [("A", "alias")],
+        ),
+        # for connections/l
+        (
+            "sql",
+            ["--connections", "--connections"],
+            [],
+        ),
+        (
+            "sql",
+            ["-l", "-l"],
+            [],
+        ),
+        (
+            "sql",
+            ["--connections", "-l"],
+            [("l", "connections")],
+        ),
+        # for close/x
+        (
+            "sql",
+            ["--close", "--close"],
+            [],
+        ),
+        (
+            "sql",
+            ["-x", "-x"],
+            [],
+        ),
+        (
+            "sql",
+            ["--close", "-x"],
+            [("x", "close")],
+        ),
+        # for mixed
+        (
+            "sql",
+            ["--creator", "--creator", "-c", "--persist", "--file", "-f", "-c"],
+            [("c", "creator"), ("f", "file")],
+        ),
+        #
+        # FOR SQLPLOT
+        #
+        # for table/t
+        (
             "sqlplot",
-            "Duplicate arguments in %sqlplot. \
-Please use only one of each of the following: --column, --table",
+            ["--table", "--table", "--column"],
+            [],
+        ),
+        (
+            "sqlplot",
+            ["-t", "-t", "--column"],
+            [],
+        ),
+        (
+            "sqlplot",
+            ["--table", "-t", "--column"],
+            [("t", "table")],
+        ),
+        # for column/c
+        (
+            "sqlplot",
+            ["--table", "--column", "--column"],
+            [],
+        ),
+        (
+            "sqlplot",
+            ["--table", "-c", "-c"],
+            [],
+        ),
+        (
+            "sqlplot",
+            ["--table", "--column", "-c"],
+            [("c", "column")],
+        ),
+        # for bins/b
+        (
+            "sqlplot",
+            ["--table", "--column", "--bins", "--bins"],
+            [],
+        ),
+        (
+            "sqlplot",
+            ["--table", "--column", "-b", "-b"],
+            [],
+        ),
+        (
+            "sqlplot",
+            ["--table", "--column", "--bins", "-b"],
+            [("b", "bins")],
+        ),
+        # for breaks/B
+        (
+            "sqlplot",
+            ["--table", "--column", "--breaks", "--breaks"],
+            [],
+        ),
+        (
+            "sqlplot",
+            ["--table", "--column", "-B", "-B"],
+            [],
+        ),
+        (
+            "sqlplot",
+            ["--table", "--column", "--breaks", "-B"],
+            [("B", "breaks")],
+        ),
+        # for binwidth/W
+        (
+            "sqlplot",
+            ["--table", "--column", "--binwidth", "--binwidth"],
+            [],
+        ),
+        (
+            "sqlplot",
+            ["--table", "--column", "-W", "-W"],
+            [],
+        ),
+        (
+            "sqlplot",
+            ["--table", "--column", "--binwidth", "-W"],
+            [("W", "binwidth")],
+        ),
+        # for orient/o
+        (
+            "sqlplot",
+            ["--table", "--column", "--orient", "--orient"],
+            [],
+        ),
+        (
+            "sqlplot",
+            ["--table", "--column", "-o", "-o"],
+            [],
+        ),
+        (
+            "sqlplot",
+            ["--table", "--column", "--orient", "-o"],
+            [("o", "orient")],
+        ),
+        # for show-numbers/S
+        (
+            "sqlplot",
+            ["--table", "--column", "--show-numbers", "--show-numbers"],
+            [],
+        ),
+        (
+            "sqlplot",
+            ["--table", "--column", "-S", "-S"],
+            [],
+        ),
+        (
+            "sqlplot",
+            ["--table", "--column", "--show-numbers", "-S"],
+            [("S", "show-numbers")],
+        ),
+        # for mixed
+        (
+            "sqlplot",
+            [
+                "--table",
+                "--column",
+                "--column",
+                "-w",
+                "--with",
+                "--show-numbers",
+                "--show-numbers",
+                "--binwidth",
+                "--orient",
+                "-o",
+                "--breaks",
+                "-B",
+            ],
+            [("o", "orient"), ("B", "breaks")],
+        ),
+        #
+        # FOR SQLCMD
+        #
+        # for schema/s
+        (
+            "sqlcmd",
+            ["--schema", "--schema"],
+            [],
+        ),
+        (
+            "sqlcmd",
+            ["-s", "-s"],
+            [],
+        ),
+        (
+            "sqlcmd",
+            ["--schema", "-s"],
+            [("s", "schema")],
+        ),
+        # for table/t
+        (
+            "sqlcmd",
+            ["--table", "--table"],
+            [],
+        ),
+        (
+            "sqlcmd",
+            ["-t", "-t"],
+            [],
+        ),
+        (
+            "sqlcmd",
+            ["--table", "-t"],
+            [("t", "table")],
+        ),
+        # for mixed
+        (
+            "sqlcmd",
+            ["--table", "-t", "-s", "-s", "--schema"],
+            [("t", "table"), ("s", "schema")],
         ),
     ],
 )
 def test_check_duplicate_arguments_raises_usageerror(
-    args, cmd_from, expected_error_message
+    check_duplicate_message_factory,
+    cmd_from,
+    args,
+    aliases,
 ):
     with pytest.raises(UsageError) as excinfo:
-        util.check_duplicate_arguments(args, cmd_from)
-    assert expected_error_message in str(excinfo.value)
+        util.check_duplicate_arguments(cmd_from, args)
+    print(str(excinfo.value))
+    assert check_duplicate_message_factory(cmd_from, args, aliases) in str(
+        excinfo.value
+    )
 
 
 @pytest.mark.parametrize(
-    "args, cmd_from",
+    "cmd_from, args",
     [
+        (["--creator"], "sql"),
+        (["-c"], "sql"),
+        (["--persist"], "sql"),
+        (["-p"], "sql"),
+        (["--persist", "--no-index"], "sql"),
+        (["--persist", "-n"], "sql"),
+        (["--file"], "sql"),
+        (["-f"], "sql"),
+        (["--save"], "sql"),
+        (["-S"], "sql"),
+        (["--alias"], "sql"),
+        (["-A"], "sql"),
+        (["--connections"], "sql"),
+        (["-l"], "sql"),
+        (["--close"], "sql"),
+        (["-x"], "sql"),
         (["--table", "--column"], "sqlplot"),
+        (["--table", "-c"], "sqlplot"),
+        (["-t", "--column"], "sqlplot"),
+        (["--table", "--column", "--breaks"], "sqlplot"),
+        (["--table", "--column", "-B"], "sqlplot"),
+        (["--table", "--column", "--bins"], "sqlplot"),
+        (["--table", "--column", "-b"], "sqlplot"),
+        (["--table", "--column", "--binwidth"], "sqlplot"),
+        (["--table", "--column", "-W"], "sqlplot"),
+        (["--table", "--column", "--orient"], "sqlplot"),
+        (["--table", "--column", "-o"], "sqlplot"),
+        (["--table", "--column", "--show-numbers"], "sqlplot"),
+        (["--table", "--column", "-S"], "sqlplot"),
+        (["--table"], "sqlcmd"),
+        (["-t"], "sqlcmd"),
+        (["--table", "--schema"], "sqlcmd"),
+        (["--table", "-s"], "sqlcmd"),
     ],
 )
-def test_check_duplicate_arguments_does_not_raise_usageerror(args, cmd_from):
-    assert util.check_duplicate_arguments(args, cmd_from)
+def test_check_duplicate_arguments_does_not_raise_usageerror(cmd_from, args):
+    assert util.check_duplicate_arguments(cmd_from, args)
