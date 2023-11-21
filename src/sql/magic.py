622fe383748375980c768b987cdcd41dc175988c
Vineet Agarwal <vineetagarwal2402@gmail.com>
2023-11-21 12:32:07 -0800

    removed path returns

--- src/sql/magic.py
+++ src/sql/magic.py
@@ -696,7 +696,7 @@ def get_query_type(command: str):
 def set_configs(ip, file_path, alternate_path):
     """Set user defined SqlMagic configuration settings"""
     sql = ip.find_cell_magic("sql").__self__
-    user_configs, loaded_from = util.get_user_configs(file_path, alternate_path)
+    user_configs = util.get_user_configs(file_path, alternate_path)
     default_configs = util.get_default_configs(sql)
     table_rows = []
 
