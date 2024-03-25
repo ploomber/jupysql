81303886ec263ed9c80747d0a73aed2532177dd5
neelasha23 <neelasha.sen@gmail.com>
2024-03-25 22:39:27 +0530

    sqlalchemy pinned

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ install_requires = [
     "prettytable",
     # IPython dropped support for Python 3.8
     "ipython<=8.12.0; python_version <= '3.8'",
-    "sqlalchemy",
+    "sqlalchemy==2.0.28",
     "sqlparse",
     "ipython-genutils>=0.1.0",
     "jinja2",
