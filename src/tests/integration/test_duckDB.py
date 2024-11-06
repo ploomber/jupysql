ad48fae9eb5c8c93dafcce6edbd02634c3922b6c
Eduardo Blancas <github@blancas.io>
2024-11-06 09:25:15 -0600

    adds test

--- src/tests/integration/test_duckDB.py
+++ src/tests/integration/test_duckDB.py
@@ -235,3 +235,10 @@ def test_commits_all_statements(ip, sql, request):
     out = ip.run_cell(sql)
     assert out.error_in_exec is None
     assert out.result.dict() == {"x": (1, 2)}
+
+
+def test_can_query_existing_df(ip_with_duckdb_sqlalchemy_empty):
+    df = pd.DataFrame({"city": ["NYC"]})
+    ip_with_duckdb_sqlalchemy_empty.run_cell("%sql SET python_scan_all_frames=true")
+    out = ip_with_duckdb_sqlalchemy_empty.run_cell("%sql SELECT * FROM df;")
+    assert out.result.dict() == {"city": ("NYC",)}
